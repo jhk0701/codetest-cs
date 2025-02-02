313d8092d326a97d26cd37f252363aa0f621b4bb
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-02-02 14:28:02 +0900

    Update: 문제 풀이

--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -11,11 +11,29 @@ namespace Test
 
         static void Main(string[] args) 
         {
-            string numbers = "123";
+            int n = 7;
+            int k = 3;
+            int[] enemy = { 4, 2, 4, 5, 3, 3, 1 };
             int answer = 0;
 
-            FindingPrime sol = new FindingPrime(numbers);
-            Console.WriteLine(sol.Count);
+            for(int i = 0; i < enemy.Length; i++) 
+            {
+                if(n < enemy[i])
+                    if(k > 0) 
+                    {
+                        answer++;
+                        k--;
+                        continue;
+                    }
+                    else 
+                        break;
+
+                n -= enemy[i];
+                answer++;
+            }
+
+            Console.WriteLine(answer);
         }
+
     }
 }
