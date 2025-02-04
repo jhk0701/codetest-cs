dc541389de1e16e87ea33fa747f67b0081e46367
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-02-04 09:58:45 +0900

    Update: 문제 풀이

---
+++ CodeTest/DefenceGame.cs
@@ -0,0 +1,44 @@
+﻿namespace Test
+{
+    public class DefenceGame 
+    {
+        public int Sol(int n, int k, ref int[] enemy)
+        {
+            int answer = 0;
+            PriorityQueue<int, int> pq = new PriorityQueue<int, int>();
+            for (int i = 0; i < enemy.Length; i++)
+            {
+                if (pq.Count < k)
+                {
+                    pq.Enqueue(enemy[i], enemy[i]);
+                    answer++;
+                    continue;
+                }
+
+
+                if (enemy[i] > pq.Peek())
+                {
+                    int small = pq.Dequeue();
+                    n -= small;
+                    pq.Enqueue(enemy[i], enemy[i]);
+
+                    answer++;
+                }
+                else
+                {
+                    n -= enemy[i];
+
+                    if (n >= 0)
+                        answer++;
+                    else
+                        break;
+                }
+            }
+
+
+            Console.WriteLine(answer);
+            return answer;
+        }
+    
+    }
+}
--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -3,37 +3,15 @@ using System.Collections.Generic;
 using CodeTest;
 using System.Collections;
 using System.Text;
+using System;
+using System.Numerics;
 
 namespace Test
 {
     internal partial class Program
     {
-
-        static void Main(string[] args) 
+        static void Main(string[] args)
         {
-            int n = 7;
-            int k = 3;
-            int[] enemy = { 4, 2, 4, 5, 3, 3, 1 };
-            int answer = 0;
-
-            for(int i = 0; i < enemy.Length; i++) 
-            {
-                if(n < enemy[i])
-                    if(k > 0) 
-                    {
-                        answer++;
-                        k--;
-                        continue;
-                    }
-                    else 
-                        break;
-
-                n -= enemy[i];
-                answer++;
-            }
-
-            Console.WriteLine(answer);
         }
-
     }
 }
