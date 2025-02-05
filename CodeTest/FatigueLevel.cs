0d32047f9b2982c0e147b8f89903ab9f26e923ed
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-02-05 10:51:55 +0900

    Update: 문제풀이

---
+++ CodeTest/FatigueLevel.cs
@@ -0,0 +1,31 @@
+﻿namespace Test
+{
+    public class FatigueLevel 
+    {
+        public int Answer { get; private set; }
+
+        public FatigueLevel(int k, ref int[,] dungeons) 
+        {
+            Answer = 0;
+            DFS(k, 0, new bool[dungeons.GetLength(0)], ref dungeons);
+        }
+
+        void DFS(int k, int cnt, bool[] visited, ref int[,] dungeons)
+        {
+            for (int i = 0; i < dungeons.GetLength(0); i++) 
+            {
+                if(k >= dungeons[i, 0] && !visited[i])
+                {
+                    bool[] dirty = new bool[visited.Length];
+                    Array.Copy(visited, dirty, visited.Length);
+                    dirty[i] = true;
+
+                    DFS(k - dungeons[i, 1], cnt + 1, dirty, ref dungeons);
+                }
+                else
+                    Answer = Math.Max(Answer, cnt);
+            }
+        }
+    }
+
+}
--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -1,11 +1,9 @@
-﻿using System.Linq;
+﻿
+using System;
+using System.Linq;
+using System.Collections;
 using System.Collections.Generic;
 using CodeTest;
-using System.Collections;
-using System.Text;
-using System;
-using System.Numerics;
-using System.Net.NetworkInformation;
 
 namespace Test
 {
@@ -13,35 +11,15 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            int[,] sizes = { {60, 50}, {30, 70}, {60, 30}, {80, 40} };
-            
-            for (int i = 0; i < sizes.GetLength(0); i++) 
-            {
-                var a = sizes[i, 0];
-                Console.WriteLine(a);
-            }
-
+            int k = 80;
+            int[,] dungeons = { {80, 20}, { 50, 40 }, {30, 10} };
             int answer = 0;
-            void DFS(int k, int idx, bool[] visited, ref int[,] dungeons)
-            {
-                if (idx >= dungeons.GetLength(0))
-                {
-                    answer = Math.Max(answer, idx);
-                    return;
-                }
 
-                for (int i = 0; i < dungeons.GetLength(0); i++)
-                {
-                    if (visited[i] || dungeons[i, 0] > k)
-                        continue;
+            FatigueLevel sol = new FatigueLevel(k, ref dungeons);
+            answer = sol.Answer;
 
-                    bool[] dirty = new bool[visited.Length];
-                    Array.Copy(visited, dirty, visited.Length);
-
-                    dirty[i] = true;
-                    DFS(k - dungeons[i, 1], idx + 1, dirty, ref dungeons);
-                }
-            }
+            Console.WriteLine(answer);
         }
     }
+
 }
