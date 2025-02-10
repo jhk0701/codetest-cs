ee419612f81efe3f982c9a2cdb157485d3fc462e
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-02-10 11:12:58 +0900

    Feat: 점프와 순간이동

---
+++ CodeTest/JumpAndTeleport.cs
@@ -0,0 +1,20 @@
+﻿namespace Test
+{
+    public class JumpAndTeleport 
+    {
+        public int Answer { get; private set; }
+        public JumpAndTeleport(int n) 
+        {
+            int Answer = 1;
+
+            while (n > 1)
+            {
+                int m = n / 2;
+                int r = n % 2;
+                if (r > 0)
+                    Answer++;
+                n = m;
+            }
+        }
+    }
+}
