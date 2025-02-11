dfa0cf89aad4e5b8e30acd01ce2294ddb28b2369
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-02-11 09:29:53 +0900

    Feat: 스킬트리

--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -8,14 +8,6 @@ namespace Test
 {
     internal partial class Program
     {
-        public class SortedQ : IComparer<KeyValuePair<int, int>>
-        {
-            public int Compare(KeyValuePair<int, int> x, KeyValuePair<int, int> y)
-            {
-                return x.Value.CompareTo(y.Value);
-            }
-        }
-
         static void Main(string[] args)
         {
         }
---
+++ CodeTest/SkillTree.cs
@@ -0,0 +1,32 @@
+﻿namespace Test
+{
+    public class SkillTree
+    {
+        public int Answer { get; private set; }
+
+        public SkillTree(string skill, ref string[] skill_trees)
+        {
+            Answer = 0;
+
+            for (int i = 0; i < skill_trees.Length; i++)
+            {
+                string curSkill = skill_trees[i];
+                int idx = 0;
+
+                for (int j = 0; j < curSkill.Length; j++)
+                {
+                    if (skill.Contains(curSkill[j]))
+                    {
+                        if (curSkill[j] == skill[idx])
+                            idx++;
+                        else
+                            break;
+                    }
+
+                    if (j == curSkill.Length - 1)
+                        Answer++;
+                }
+            }
+        }
+    }
+}
