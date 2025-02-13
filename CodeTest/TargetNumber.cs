5d8b3e5db12002aac7702d127ec5ec893c309630
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-02-13 10:33:14 +0900

    Feat: 타겟 넘버

---
+++ CodeTest/TargetNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace Test
+{
+    public class TargetNumber 
+    {
+        public int Answer { get; private set; }
+
+        public TargetNumber(int[] numbers, int target) 
+        {
+            Answer = 0;
+
+            DFS(0, 0, ref target, ref numbers);
+        }
+
+        void DFS(int sum, int idx, ref int target, ref int[] nums)
+        {
+            if (idx >= nums.Length)
+            {
+                if (sum == target)
+                    Answer++;
+
+                return;
+            }
+
+            DFS(sum + nums[idx], idx + 1, ref target, ref nums);
+            DFS(sum - nums[idx], idx + 1, ref target, ref nums);
+        }
+    }
+}
