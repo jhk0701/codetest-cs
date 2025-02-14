ec686f22d7c693ab5e8c890d455d8cdcb9d75a74
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-02-14 17:01:24 +0900

    Feat: 문자열 나누기

---
+++ CodeTest/SplitString.cs
@@ -0,0 +1,45 @@
+﻿namespace Test
+{
+    public class SplitString 
+    {
+        public int Answer { get; private set; }
+
+        public SplitString(string s) 
+        {
+            Answer = 0;
+
+            char c = '1';
+            int dup = 0;
+            int notDup = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (c == '1')
+                {
+                    c = s[i];
+                    dup = 1;
+                    continue;
+                }
+
+                if (s[i] == c)
+                {
+                    dup++;
+                    continue;
+                }
+
+                notDup++;
+
+                if (dup == notDup)
+                {
+                    Answer++;
+                    dup = 0;
+                    notDup = 0;
+                    c = '1';
+                }
+            }
+
+            if (c != '1')
+                Answer++;
+        }
+    }
+}
