554344d3ced6293651454d5893a8a15e2e4cb82d
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-02-28 17:23:39 +0900

    Feat: 3진법 뒤집기

---
+++ CodeTest/Flip3Digit.cs
@@ -0,0 +1,34 @@
+﻿namespace Test
+{
+    public class Flip3Digit 
+    {
+        public int Sol(int n) 
+        {
+            int answer = 0;
+            int digit = 3;
+
+            string converted = "";
+
+            while (n >= 3)
+            {
+                int r = n % 3;
+                converted += r;
+
+                n = n / 3;
+            }
+
+            converted += n;
+
+            for (int i = 0; i < converted.Length; i++)
+            {
+                int idx = converted.Length - 1 - i;
+                if (converted[idx] == '0')
+                    continue;
+
+                answer += int.Parse(converted[idx].ToString()) * (int)Math.Pow(3, i);
+            }
+
+            return answer;
+        }
+    }
+}
--- CodeTest/Program.cs
+++ CodeTest/Program.cs
@@ -15,29 +15,36 @@ namespace Test
 
             ArcheryCompatition archeryCompatition = new ArcheryCompatition();
             Console.WriteLine(string.Join(",", archeryCompatition.Sol(n, info)));
+            Console.WriteLine(archeryCompatition.SearchCnt);
         }
     }
 
     public class ArcheryCompatition 
     {
+        public int SearchCnt;
+
         bool isWin;
         int diff;
         public int[] Result;
 
         public int[] Sol(int n, int[] info)
         {
+            SearchCnt = 0;
+
             isWin = false;
             diff = int.MinValue;
             Result = new int[info.Length];
 
             int[] arr = new int[info.Length];
-            Search(0, n, ref arr, ref info);
+            Search(0, n, ref arr, ref info, 0);
 
             return isWin ? Result : new int[]{-1};
         }
 
-        void Search(int idx, int cnt, ref int[] arr, ref int[] apeach)
+        void Search(int idx, int cnt, ref int[] arr, ref int[] apeach, int score)
         {
+            SearchCnt++;
+            Console.WriteLine($"{SearchCnt} : {string.Join(",", arr)}");
 
             if (idx >= cnt)
             {
@@ -54,22 +61,23 @@ namespace Test
                 return;
             }
 
-
-
             for(int i = 0; i < arr.Length; i++) 
             {
                 arr[i] += 1;
 
-                Search(idx + 1, cnt, ref arr, ref apeach);
+                int newScore = CalcScore(ref arr, ref apeach);
+                Search(idx + 1, cnt, ref arr, ref apeach, newScore);
 
                 arr[i] -= 1;
             }
-        }
 
+            Console.WriteLine("Break");
+        }
 
         bool IsWin(ref int[] ryan, ref int[] apeach, out int diff) 
         {
             int ryanNum = 0, apeachNum = 0;
+
             for (int i = 0; i < apeach.Length; i++) 
             {
                 if (ryan[i] == 0 && apeach[i] == 0)
@@ -80,9 +88,26 @@ namespace Test
                 else
                     apeachNum += 10 - i;
             }
+
             bool result = ryanNum > apeachNum;
             diff = result ? ryanNum - apeachNum : 0;
             return result;
+        }
+
+        int CalcScore(ref int[] ryan, ref int[] apeach) 
+        {
+            int ryanNum = 0;
+
+            for (int i = 0; i < apeach.Length; i++)
+            {
+                if (ryan[i] == 0 && apeach[i] == 0)
+                    continue;
+
+                if (ryan[i] > apeach[i])
+                    ryanNum += 10 - i;
+            }
+
+            return ryanNum;
         }
         
         bool Compare(ref int[] arr1, ref int[] arr2) 
