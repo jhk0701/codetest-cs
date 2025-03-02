29bf73eb19f790b91aac445f9dc287d55d40442f
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-03-02 08:05:48 +0000

    Merge cpp

---
+++ CodeTest-cpp/NAndM_2.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+
+using namespace std;
+
+void Search(int m, vector<bool>& is_visited, vector<int>& arr)
+{
+    if(arr.size() == m)
+    {
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if (i > 0)
+                cout << ' ';
+
+            cout << arr[i];
+        }
+
+        cout << '\n';
+        return;
+    }
+
+    for(int i = 0; i < is_visited.size(); i++)
+    {
+        if(is_visited[i])
+            continue;
+        if(arr.size() > 0 && arr.back() > i + 1)
+            continue;
+            
+        arr.push_back(i + 1);
+        is_visited[i] = true;
+
+        Search(m, is_visited, arr);
+
+        arr.pop_back();
+        is_visited[i] = false;
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cout.tie(0);
+    cin.tie(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<bool> is_visited;
+    vector<int> ansArr;
+
+    for(int i = 0; i < n; i++)
+        is_visited.push_back(false);
+
+    Search(m, is_visited, ansArr);
+}
+
