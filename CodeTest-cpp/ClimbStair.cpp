8a383e838f77a5ee4a241b0106c0065bb2d7f445
jhk0701 <134210309+jhk0701@users.noreply.github.com>
2025-03-14 13:03:02 +0000

    Update: 계단오르기 수정

--- CodeTest-cpp/ClimbStair.cpp
+++ CodeTest-cpp/ClimbStair.cpp
@@ -9,24 +9,32 @@ int main()
     cin.tie(0);
     cout.tie(0);
 
+    int stair[300];
+    int arr[300][2];
+
     int n;
     cin >> n;
+
+    for(int i = 0; i < n; i++) 
+        cin >> stair[i];
     
-    int arr[n];
-    vector<vector<int>> d;
-
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-
-    d.push_back({arr[0], 0, 1});
-
-    for(int i = 1; i <= n - 1; i++)
+    if(n == 1)
+        cout << stair[n - 1] << endl;
+    else
     {
-        int val = d[i - 1][0];
-        int prevIdx = d[i - 1][1];
-        int cnt = d[i - 1][2];
+        arr[0][0] = stair[0];
+        arr[0][1] = 0;
         
-        
+        arr[1][0] = stair[1];
+        arr[1][1] = stair[1] + stair[0];
+
+        for(int i = 2; i < n; i++)
+        {
+            arr[i][0] = max(arr[i-2][0], arr[i-2][1]) + stair[i];
+            arr[i][1] = arr[i-1][1] + stair[i];
+        }
+
+        cout << max(arr[n - 1][0], arr[n - 1][1]) << endl;
     }
 
     return 0;
